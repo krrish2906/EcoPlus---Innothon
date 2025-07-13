84b4c54d0afd2facd5e8fc1c5da31dc86412dddd
Aarya Agrawal <aarya6105@gmail.com>
2025-07-14 00:07:48 +0530

    Auth completed

--- backend/index.js
+++ backend/index.js
@@ -2,29 +2,30 @@ import express from 'express'
 import dotenv from 'dotenv'
 
 import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import { connectDb } from './lib/db.js';
+// import messageRoutes from './routes/message.route.js'
+dotenv.config();
+import { connectDb } from './config/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
-import { app ,server} from './lib/socket.js';
+// import { app ,server} from './lib/socket.js';
 
-dotenv.config();
 const PORT = process.env.PORT;
-
+const app = express();
 app.use(express.json())
 app.use(cookieParser());
+
 app.use(cors({
 
-    origin:"http://localhost:5173",
+    origin:"http://localhost:3000",
     credentials :true
 
 })
 )
 app.use("/api/auth" , authRoutes);
-app.use("/api/message" , messageRoutes);
+
 const setUpAndStartServer = ()=>{
 
-    server.listen(PORT , ()=>{
+    app.listen(PORT , ()=>{
         console.log("app is listening on the port " , PORT);
         connectDb();
     })
