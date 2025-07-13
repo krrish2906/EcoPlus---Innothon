267e6bdde0b5c5f818fb3a864991f58ee97df20b
Aarya Agrawal <aarya6105@gmail.com>
2025-07-13 23:20:28 +0530

    Auth added

---
+++ backend/controllers/authController.js
@@ -0,0 +1,113 @@
+
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs'
+import { generateToken } from '../lib/utils.js';
+import cloudinary from '../lib/cloudinary.js'
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    try {
+        if (password.length < 6) {
+            return res.status(400).json({ message: "The password must be more than 6 letters" })
+        }
+
+        const user = await User.findOne({ email });
+        if (user) return res.status(400).json({ message: "User already exists" });
+
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            fullName: fullName,
+            password: hashedPassword,
+            email: email
+        })
+
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({ _id: newUser._id, fullName: newUser.fullName, email: newUser.email, profilePic: newUser.profilePic })
+        }
+        else {
+            return res.status(400).json({ message: "invalid user data" })
+        }
+
+    } catch (error) {
+        console.log(
+            "Error in signup controller ", error.message
+        )
+        return res.status(500).json({ message: "Internal  Server Error" })
+    }
+
+}
+export const login = async (req, res) => {
+    const { password, email } = req.body;
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ message: "The user does not exist " })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res.status(401).json({ message: "Invalid credentials " })
+        }
+
+        generateToken(user._id, res);
+        return res.status(200).json({ _id: user._id, fullName: user.fullName, email: user.email, profilePic: user.profilePic })
+
+
+    } catch (error) {
+            console.log("Error in the login controller" , error.message);
+
+            return res.status(500).json({ message: "Internal  Server Error" })
+    }
+
+}
+export const logout = (req, res) => {
+    try {
+        res.cookie("jwt" , "" , {maxAge : 0});
+        return res.status(200).json({message : "logged out successfully"})
+    } catch (error) {
+         console.log("Error in the login controller" , error.message);
+
+            return res.status(500).json({ message: "Internal  Server Error" })
+    }
+
+}
+
+export const updateProfile= async (req, res)=>{
+
+    try {
+        // console.log(req.body)
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message : "profile pic is required"});
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId , {profilePic : uploadResponse.secure_url} ,{new:true})
+         return res.status(200).json(updatedUser);
+    } catch (error) { 
+        console.log("Error in the updateProfile controller" , error.message);
+
+            return res.status(500).json({ message: "Internal  Server Error" })
+    }
+
+    
+}
+
+export const checkAuth=  async(req, res)=>{
+    try {
+        return res.status(200).json(req.user)
+    } catch (error) {
+        console.log("Error in the checkAuth controller" , error.message);
+
+            return res.status(500).json({ message: "Internal  Server Error" })
+    }
+
+}
---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
+import { connectDb } from './lib/db.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import { app ,server} from './lib/socket.js';
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use(express.json())
+app.use(cookieParser());
+app.use(cors({
+
+    origin:"http://localhost:5173",
+    credentials :true
+
+})
+)
+app.use("/api/auth" , authRoutes);
+app.use("/api/message" , messageRoutes);
+const setUpAndStartServer = ()=>{
+
+    server.listen(PORT , ()=>{
+        console.log("app is listening on the port " , PORT);
+        connectDb();
+    })
+}
+
+setUpAndStartServer();
