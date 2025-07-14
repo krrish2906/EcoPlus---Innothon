b3d1a2b01adb4527fd3be4c18d249dde48a2d798
Aarya Agrawal <aarya6105@gmail.com>
2025-07-14 19:47:00 +0530

    Added post model

---
+++ backend/models/post.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    report : {
+       
+        title : String,
+        description : String,
+        imageUrl : String,
+        location : {
+            lat: Number,
+            lng: Number,
+            address: String
+          },
+
+    },
+    category : {
+        type : String,
+        required : true,
+        enum : ['Garbage', 'Plastic', 'Sewage','Waste']
+    },
+    status : {
+        type : String,
+        required : true,
+        enum : ['Pending', 'Resolved']
+    },
+    createdBy : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    },
+    comments:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Comment"
+    },
+    escalatedAt : {
+        type : Date,
+        default : null
+    },
+    
+
+} , {timestamps : true})
+
+const Post = mongoose.model("Post" , postSchema)
+
+export default Post
