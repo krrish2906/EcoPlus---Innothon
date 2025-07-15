5c127c53b136e9c26482df60aee3a6a5cbf44c45
krrish2906 <krrishbansal290605@gmail.com>
2025-07-15 19:45:59 +0530

    comment module done

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -49,5 +49,5 @@ const userSchema = new mongoose.Schema(
      , {timestamps : true}
 )
 
-const User = mongoose.model('User ' , userSchema) ;
+const User = mongoose.model('User' , userSchema) ;
 export default User ;    
