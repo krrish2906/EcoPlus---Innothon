499505d3af2f70fec81a987f8a188d32e495dffc
Prathamesh170305 <wankhadeprathamesh17@gmail.com>
2025-07-16 23:56:37 +0530

    Added homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,15 @@ import {Routes, Route} from 'react-router-dom'
 import PostForm from './pages/postForm'
 import SignUpPage from './pages/SignUp'
 import LogInPage from './pages/Login'
+import Home from './pages/Home'
+
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<SignUpPage />} />
+      <Route path="/Home" element={<Home/>}/>
+      <Route path="/postform" element={<PostForm/>}/>
     </Routes>
   )
 }
