60e80a0b0bbc33178f2b7709c82fd52e8a6dd179
Aarya Agrawal <aarya6105@gmail.com>
2025-07-20 17:56:04 +0530

    Added geolocation logic

--- frontend/src/pages/PostsPage.jsx
+++ frontend/src/pages/PostsPage.jsx
@@ -2,39 +2,11 @@ import React from "react"
 import { useNavigate } from "react-router-dom"
 import SideBar from "../components/SideBar"
 
-const posts = [
-  {
-    author: "Green Earth Initiative",
-    title: "Community Cleanup Event",
-    description:
-      "Join us for a community cleanup event at Central Park on Saturday, July 20th. We'll be picking up trash, planting trees, and making our park a better place for everyone. All ages welcome!",
-    image: "/images/cleanup.jpg",
-  },
-  {
-    author: "Sarah Miller",
-    title: "Fundraising Bake Sale",
-    description:
-      "I'm organizing a bake sale to raise funds for the local animal shelter. Come by the town square on Sunday, July 21st, and support a great cause with some delicious treats!",
-    image: "/images/bake-sale.jpg",
-  },
-  {
-    author: "Community Helpers",
-    title: "Elderly Assistance Program",
-    description:
-      "We're looking for volunteers to assist elderly residents with errands, companionship, and light household tasks. If you have some free time and a caring heart, please sign up!",
-    image: "/images/elderly-help.jpg",
-  },
-  {
-    author: "Animal Rescue League",
-    title: "Pet Adoption Day",
-    description:
-      "Join us for our pet adoption day at the local pet store on Saturday, July 27th. Find your new furry friend and give a loving home to an animal in need!",
-    image: "/images/pet-adoption.jpg",
-  },
-]
+
 
 export default function Home() {
     const navigate = useNavigate();
+
   return (
     <div className="flex h-screen overflow-hidden">
       <SideBar />
